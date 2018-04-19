e50c59b62b3f7e04cc2f15fbde65aed758a73ec0
Eric Schoonover <eric@teamsodo.com>
2018-04-19 00:40:12 -0700

    removing unecessary aws and backend tf files

--- modules/app/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    profile              = "synacor"
-    bucket               = "zm-tf-state"
-    workspace_key_prefix = "env"
-    key                  = "app.tfstate"
-    region               = "us-east-1"
-  }
-}
