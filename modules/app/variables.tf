20da09cbd48b0e88ecd5ecb06fc736fb87c269eb
Eric Schoonover <eric@teamsodo.com>
2018-04-19 23:51:11 -0700

    create a container repository for each zm-* container

--- modules/app/variables.tf
+++ modules/app/variables.tf
@@ -2,10 +2,23 @@ locals {
   environment = "${terraform.workspace}"
 }
 
-variable "blockchain_vpc_id" {}
-variable "blockchain_public_subnet_id" {}
+variable "blockchain_vpc_id" {
+  type = "string"
+}
+
+variable "blockchain_public_subnet_id" {
+  type = "string"
+}
 
 locals {
   blockchain_vpc_id           = "${var.blockchain_vpc_id}"
   blockchain_public_subnet_id = "${var.blockchain_public_subnet_id}"
+}
+
+variable "containers" {
+  type = "list"
+}
+
+locals {
+  containers = "${var.containers}"
 }
--- zimbrax.tf
+++ zimbrax.tf
@@ -6,6 +6,18 @@ module "app" {
   source                      = "./modules/app"
   blockchain_vpc_id           = "${module.platform.blockchain_vpc_id}"
   blockchain_public_subnet_id = "${module.platform.blockchain_public_subnet_id}"
+
+  containers = [
+    "redis",
+    "zmc-ldap",
+    "zmc-mta",
+    "zmc-mailbox",
+    "zmc-account",
+    "zm-x-web",
+    "zmc-mysql",
+    "zmc-proxy",
+    "zmc-solr",
+  ]
 }
 
 output "blockchain_dev_public_ip" {
