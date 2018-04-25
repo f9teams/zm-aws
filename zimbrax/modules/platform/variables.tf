469e53a8d2f6c431e4675679386636818fa76081
Eric Schoonover <eric@teamsodo.com>
2018-04-25 01:05:24 -0700

    add docker repos to output

--- zimbrax/modules/platform/variables.tf
+++ zimbrax/modules/platform/variables.tf
@@ -8,8 +8,8 @@ variable "containers" {
 
 locals {
   environment  = "${terraform.workspace}"
-  env_prefix_d = "${terraform.workspace == "prod" ? "" : terraform.workspace}-"
-  env_prefix_u = "${terraform.workspace == "prod" ? "" : terraform.workspace}_"
+  env_prefix_d = "${terraform.workspace == "prod" ? "" : "${terraform.workspace}-"}"
+  env_prefix_u = "${terraform.workspace == "prod" ? "" : "${terraform.workspace}_"}"
 
   r53_zone_id = "${var.r53_zone_id}"
   containers  = "${var.containers}"
--- zimbrax/outputs.tf
+++ zimbrax/outputs.tf
@@ -5,3 +5,7 @@ output "bastion_fqdn" {
 output "dockerhost_fgdn" {
   value = "${module.swarm.dockerhost_fqdn}"
 }
+
+output "docker_repositories" {
+  value = "${module.platform.docker_repositories}"
+}
